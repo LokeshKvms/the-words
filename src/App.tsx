07c840c8c68ed26dd109b54c4930a2c92c92b8e0
LokeshKvms <loki.kvms@gmail.com>
2025-06-27 10:10:11 +0530

    Project Setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+// src/App.tsx
+function App() {
+  return (
+    <div className="min-h-screen bg-white dark:bg-gray-900 text-black dark:text-white p-6">
+      <h1 className="text-2xl font-bold">Dictionary App</h1>
+    </div>
+  );
+}
+
+export default App;
